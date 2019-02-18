6e0f813568448beda1b75c387597fd163877ca10
yashraikar1087 <yashraikar1087@gmail.com>
2019-02-18 10:59:24 -0700

    add axios and async

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { images: [] };
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization:
+          "Client-ID 6eef543f991f310cd2721f9c447bdad883d480a56f00b8c150b42f3a18565654"
+      }
+    });
+    this.setState({ images: response.data.results });
+  };
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
+      </div>
+    );
+  }
+}
 
 export default App;
